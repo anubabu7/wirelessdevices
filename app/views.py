aff23b2a40b941e24b37f1f3b03aef801de53aaa
AnuBabu <anubabukylm@gmail.com>
2023-09-04 13:37:11 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render,redirect
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return render(request,"index.html")
+def createaccount(request):
+    return render(request,"createaccount.html")
+def user1(request):
+    return render(request,"user1.html")
+def admin1(request):
+    return render(request,"admin1.html")
