86861968050abbbd914ec649ea22d789beb09bde
Arathi <pythontrainer.tecswan@gmail.com>
2023-09-07 09:52:19 +0530

    design

--- app/views.py
+++ app/views.py
@@ -7,11 +7,11 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-     #return render(request,"index.html")
+     return render(request,"index.html")
    # return render(request,"login1.html")
-    #return render(request,"adminHome.html")
-    #return render(request,"staffHome.html")
-    return render(request,"sellerHome.html")
+    # return render(request,"adminHome.html")
+    # return render(request,"staffHome.html")
+    # return render(request,"sellerHome.html")
    # return render(request,"userHome.html")
 def createAccount(request):
     return render(request,"createAccount.html")
